cb0334f95393d393d6ac68c99362072aed7d7424
Leo Gao <leo.gao@dbschenker.com>
2024-09-14 23:26:45 +0800

    add decorator dp:refactor

--- chapter03/6.4-decoratordesignpattern/decorator/component.go
+++ chapter03/6.4-decoratordesignpattern/decorator/component.go
@@ -1,5 +1,14 @@
 package decoratordesignpattern
 
+import "fmt"
+
 type Component interface {
 	Operation()
+}
+
+type ConcreteComponent struct {
+}
+
+func (c *ConcreteComponent) Operation() {
+	fmt.Println("具体对象的实际操作")
 }
