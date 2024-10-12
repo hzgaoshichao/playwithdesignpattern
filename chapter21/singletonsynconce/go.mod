b6aeb7b2669a32bc52884ed0121cec077739894c
Leo Gao <leo.gao@dbschenker.com>
2024-10-12 22:55:04 +0800

    singletonpattern:init

---
+++ chapter21/singletonsynconce/go.mod
@@ -0,0 +1,3 @@
+module singletonsynconce
+
+go 1.22.3
---
+++ chapter21/singletonsynconce/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"singletonsynconce/singleton"
+)
+
+func main() {
+	instance := singleton.GetInstance()
+
+	fmt.Println(instance)
+}
+
+// 参考链接
+// https://www.liwenzhou.com/posts/Go/singleton/
+
+// 关于原子操作
+// https://blog.csdn.net/chai2010/article/details/120426568
+// https://stackoverflow.com/questions/55840399/golang-what-is-atomic-read-used-for
